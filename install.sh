35a1adcfbdd9b50bc4a4bbdf3c7cc246e2b71454
Patrick Grossmann <patrickdgro@gmail.com>
2022-08-10 00:18:32 +0200

    fix: dracut option confdir no longe accepts dev/null auto: kernel update

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+function die() {
+	exit 1
+}
+
 old=$1
 new=$2
 kver="$(realpath "$new")"
@@ -5,14 +9,14 @@ kver="$(basename "$kver")"
 kver="${kver#linux-}"
 umask 022
 export MAKEFLAGS="-j9"
-cp "$old"/.config "$new"/.config
-cd "$new"
-rm ../config
-ln .config ../config
-make olddefconfig
-make
-eselect kernel set linux-"$kver"
-emerge -v @module-rebuild
-make modules_install
-cp arch/x86/boot/bzImage /boot/EFI/EFI/Linux/vmlinuz.efi
-dracut --conf "/dev/null" --confdir "/dev/null" --kver "$kver" --zstd --no-hostonly --ro-mnt --add "bash zfs" --force "/boot/EFI/EFI/Linux/initramfs.img"
+cp "$old"/.config "$new"/.config || die
+cd "$new" || die
+rm ../config || die
+ln .config ../config || die
+make olddefconfig || die
+make || die
+eselect kernel set linux-"$kver" || die
+emerge -v @module-rebuild || die
+make modules_install || die
+cp arch/x86/boot/bzImage /boot/EFI/EFI/Linux/vmlinuz.efi || die
+dracut --kver "$kver" --zstd --no-hostonly --ro-mnt --add "bash zfs" --force "/boot/EFI/EFI/Linux/initramfs.img" || die
